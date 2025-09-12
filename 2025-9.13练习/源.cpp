81146ce0f5c060d5caa8e29d0096118458a64cc3
孤子赤心 <2538661745@qq.com>
2025-09-13 00:12:47 +0800

    leetcode 605.种花问题

---
+++ 2025-9.13练习/源.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& f, int n) {
+        int cnt = 0, m = f.size();
+        for (int i = 0; i < m; ++i) {
+            if ((i == 0 || f[i - 1] == 0) && f[i] == 0 && (i == m - 1 || f[i + 1] == 0)) {
+                cnt += 1;
+                i++;
+            }
+        }
+        return cnt >= n;
+    }
+};
