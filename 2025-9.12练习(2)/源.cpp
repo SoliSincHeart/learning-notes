a24c02a5426c56290be74d552a587c5076deed94
孤子赤心 <2538661745@qq.com>
2025-09-13 21:50:43 +0800

    leetcode 435.无重叠区间

--- 2025-9.12练习(2)/源.cpp
+++ 2025-9.12练习(2)/源.cpp
@@ -9,19 +9,19 @@ public:
         sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
            return a[1] < b[1];
             });
-        int count = 0;
+        int counts = 0;
         int prev_end = intervals[0][1];
         for (int i = 1; i < n; i++)
         {
             if (intervals[i][0] < prev_end)
             {
-                count++;
+                counts++;
             }
             else
             {
                 prev_end = intervals[i][1];
             }
         }
-        return count;
+        return counts;
     }
 };
