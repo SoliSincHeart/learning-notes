d067d2a7165dedd7e1ee72dd4ad0009f708ddabd
孤子赤心 <2538661745@qq.com>
2025-09-13 21:54:56 +0800

    leetcode 452. 用最少数量的箭引爆气球

--- 2025-9.13练习(2)/源.cpp
+++ 2025-9.13练习(2)/源.cpp
@@ -6,7 +6,7 @@ public:
         sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
             return a[1] < b[1];
             });
-        int arrow_pos = INT_MIN;
+        int arrow_pos = 0;
         for (int i = 0; i < points.size(); ) {
             arrow_pos = points[i][1];
             do {
