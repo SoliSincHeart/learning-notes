559be0902642ecae940e7673a50a331db2f28514
孤子赤心 <2538661745@qq.com>
2025-09-18 00:23:04 +0800

    leetcode 167.两数之和 II - 输入有序数组

---
+++ 2025-9.17练习/源.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0;
+        int sum = 0;
+        int r = numbers.size() - 1;
+        while (l < r)
+        {
+            sum = numbers[l] + numbers[r];
+            if (sum == target)break;
+            if (sum < target)++l;
+            else --r;
+        }
+        return vector<int>{l + 1, r + 1};
+    }
+};
