b9b612f2c6ddb3d4196626edd5ada8579c86de56
孤子赤心 <2538661745@qq.com>
2025-10-30 00:19:26 +0800

    leetcode 221.最大正方形

--- 2025-10.29练习/源.cpp
+++ 2025-10.29练习/源.cpp
@@ -1,3 +1,8 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
 int maximalSquare(vector<vector<char>>& matrix) {
 	if (matrix.empty() || matrix[0].empty()) {
 		return 0;
@@ -6,11 +11,35 @@ int maximalSquare(vector<vector<char>>& matrix) {
 	vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 	for (int i = 1; i <= m; ++i) {
 		for (int j = 1; j <= n; ++j) {
-			if (matrix[i - 1][j - 1] == ¡¯1¡¯) {
+			if (matrix[i - 1][j - 1] == '1') {
+				cout<<matrix[i - 1][j - 1]<<endl;
 				dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
+				cout << dp[i][j] << endl;
+				for (int k = 0; k <= m; ++k)
+				{
+					for (int l = 0; l <= n; ++l)
+					{
+						cout<<dp[k][l]<<" ";
+					}
+					cout<<endl;
+				}
 			}
 			max_side = max(max_side, dp[i][j]);
 		}
 	}
 	return max_side * max_side;
+}
+
+int main() {
+
+	vector<vector<char>> matrix = { {'0','1','1','0'},
+									{'0','1','1','1'},
+									{'0','1','1','1'},
+									{'0','1','1','1'}
+									};
+
+
+	cout << maximalSquare(matrix) << endl;
+
+	return 0;
 }
