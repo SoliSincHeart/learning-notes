16fb18adfc6c258eacdb2372e971ad6a525ebbc2
孤子赤心 <2538661745@qq.com>
2025-11-03 22:36:22 +0800

    leetcode 139.单词拆分

---
+++ 2025-11.3练习/源.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<int> dp(n + 1, false);
+        dp[0] = true;
+        for (int i = 1; i <= n; ++i)
+        {
+            for (const string& word : wordDict)
+            {
+                int len = word.length();
+                if (len <= i && s.substr(i - len, len) == word)
+                    dp[i] = dp[i] || dp[i - len];
+            }
+        }
+        return dp[n];
+    }
+};
