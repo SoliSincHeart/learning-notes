694f6ba81d24dda13d834e47f09195a6eafd8632
孤子赤心 <2538661745@qq.com>
2025-11-03 22:53:37 +0800

    leetcode 300.最长递增子序列

---
+++ 2025-10.31练习/源.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 0) return n;
+        vector<int> dp;
+        dp.push_back(nums[0]);
+        for (int i = 1; i < n; ++i)
+        {
+            if (dp.back() < nums[i])
+            {
+                dp.push_back(nums[i]);
+            }
+            else
+            {
+                auto it = lower_bound(dp.begin(), dp.end(), nums[i]);
+                *it = nums[i];
+            }
+        }
+        return dp.size();
+    }
+};
