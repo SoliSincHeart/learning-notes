2db9f19669c1143126d9304f04b20fa133032687
孤子赤心 <207161254+SoliSincHeart@users.noreply.github.com>
2026-01-09 23:03:30 +0800

    leetcode 67.二进制求和

---
+++ 2026-1.4练习/源.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string addBinary(string a, string b)
+    {
+        int al = a.size();
+        int bl = b.size();
+        while (al < bl)
+        {
+            a = '0' + a;
+            ++al;
+        }
+        while (bl < al)
+        {
+            b = '0' + b;
+            ++bl;
+        }
+        for (int i = al - 1; i > 0; --i)
+        {
+            a[i] = a[i] - '0' + b[i];
+            if (a[i] >= '2')
+            {
+                a[i] = (a[i] - '0') % 2 + '0';
+                a[i - 1] = a[i - 1] + 1;
+            }
+        }
+        a[0] = a[0] - '0' + b[0];
+        if (a[0] >= '2')
+        {
+            a[0] = (a[0] - '0') % 2 + '0';
+            a = '1' + a;
+        }
+        return a;
+    }
+};
